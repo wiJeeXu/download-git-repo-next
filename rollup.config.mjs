4d22653b3838a924b5989cc450f6d083ce9039f5
xuweijie <1243087518@qq.com>
2023-09-08 03:29:23 +0800

    feat: framework

---
+++ rollup.config.mjs
@@ -0,0 +1,14 @@
+import typescript from 'rollup-plugin-typescript2'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    dir: 'dist',
+    format: 'cjs',
+  },
+  plugins: [commonjs(), json(), typescript(), resolve()],
+  external: ['download'],
+}
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import download from 'download'
+
+download(
+  'https://github.com/vitejs/vite/archive/refs/heads/main.zip',
+  'test/temp'
+)
