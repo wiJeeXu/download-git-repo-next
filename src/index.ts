b11f2428b65a3fbe26104067e989c91df171c18a
xuweijie <1243087518@qq.com>
2023-09-09 06:36:00 +0800

    feat: downloadGit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,49 @@
 import download from 'download'
 
-download(
-  'https://github.com/vitejs/vite/archive/refs/heads/main.zip',
-  'test/temp'
-)
+// download(
+//   'https://github.com/vitejs/vite/archive/refs/heads/main.zip',
+//   'test/temp'
+// )
+
+interface Option {
+  clone?: boolean
+}
+
+function downloadGit(url: string, dest: string): void
+function downloadGit(url: string, dest: string, option: Option): void
+function downloadGit(
+  url: string,
+  dest: string,
+  fn: (err?: unknown) => void
+): void
+
+function downloadGit(
+  url: string,
+  dest: string,
+  option: Option,
+  fn: (err?: unknown) => void
+): void
+
+function downloadGit(
+  url: string,
+  dest: string,
+  optionOrFn?: Option | ((err?: unknown) => void) | null,
+  fn?: (err?: unknown) => void
+) {
+  if (typeof optionOrFn === 'function') {
+    fn = optionOrFn
+    optionOrFn = null
+  }
+  optionOrFn = optionOrFn || {}
+  const clone = optionOrFn.clone || false
+  if (clone) {
+  } else {
+  }
+}
+
+// function normalize(url: string) {
+//   /^(https:)()/
+// }
+
+export { downloadGit }
+export default downloadGit
